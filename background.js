9b7aa0928849e656ecbc65b90dc7133c39162ac4
Alexander Levin <zurdanium@gmail.com>
2020-05-21 12:53:43 +0200

    Initial commit

---
+++ background.js
@@ -0,0 +1,40 @@
+//taken from https://stackoverflow.com/a/18455088
+const copyToClipboard = text => {
+  //Create a textbox field where we can insert text to. 
+  const copyFrom = document.createElement("textarea");
+
+  //Set the text content to be the text you wished to copy.
+  copyFrom.textContent = text;
+
+  //Append the textbox field into the body as a child. 
+  //"execCommand()" only works when there exists selected text, and the text is inside 
+  //document.body (meaning the text is part of a valid rendered HTML element).
+  document.body.appendChild(copyFrom);
+
+  //Select all the text!
+  copyFrom.select();
+
+  //Execute command
+  document.execCommand('copy');
+
+  //(Optional) De-select the text using blur(). 
+  copyFrom.blur();
+
+  //Remove the textbox field from the document.body, so no other JavaScript nor 
+  //other elements can get access to this.
+  document.body.removeChild(copyFrom);
+	
+}
+
+chrome.browserAction.onClicked.addListener(tab => {
+	const currentPage = tab.url;
+	const escaped = encodeURIComponent(currentPage);
+	fetch(`https://api.song.link/v1-alpha.1/links?url=${escaped}`)
+		.then(response => response.json())
+		.then(data => {
+			const url = data.pageUrl;
+			copyToClipboard(url);
+			console.log(url);
+			//chrome.browserAction.setPopup({popup: 'popup.html'});
+		});
+});
