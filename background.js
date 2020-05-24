09d443cd362fb026b387258e16a381b477381b97
Alexander Levin <zurdanium@gmail.com>
2020-05-24 18:22:49 +0200

    Add Google Play Music and Spotify support, add JSDoc.

--- background.js
+++ background.js
@@ -1,43 +1,59 @@
-//taken from https://stackoverflow.com/a/18455088
+// taken from https://stackoverflow.com/a/18455088
 const copyToClipboard = text => {
-    //Create a textbox field where we can insert text to.
+    // Create a textbox field where we can insert text to.
     const copyFrom = document.createElement("textarea");
 
-    //Set the text content to be the text you wished to copy.
+    // Set the text content to be the text you wished to copy.
     copyFrom.textContent = text;
 
-    //Append the textbox field into the body as a child.
-    //"execCommand()" only works when there exists selected text, and the text is inside
-    //document.body (meaning the text is part of a valid rendered HTML element).
+    // Append the textbox field into the body as a child.
+    // "execCommand()" only works when there exists selected text, and the text is inside
+    // document.body (meaning the text is part of a valid rendered HTML element).
     document.body.appendChild(copyFrom);
 
-    //Select all the text!
+    // Select all the text!
     copyFrom.select();
 
-    //Execute command
+    // Execute command
     document.execCommand('copy');
 
-    //(Optional) De-select the text using blur().
+    // (Optional) De-select the text using blur().
     copyFrom.blur();
 
-    //Remove the textbox field from the document.body, so no other JavaScript nor
-    //other elements can get access to this.
+    // Remove the textbox field from the document.body, so no other JavaScript nor
+    // other elements can get access to this.
     document.body.removeChild(copyFrom);
 
 }
 
+const regexToLink = (regex, idToLink) => ({regex, idToLink});
+
+/**
+ * @param {string} tabUrl
+ * @returns {string | undefined} Odesli link if it can be derived from url or undefined otherwise
+ */
 const generateOdesliLink = tabUrl => {
-    const regexToLink = (regex, idToLink) => ({regex, idToLink});
     const regexToLinkList = [
         regexToLink(/.*music.yandex.ru\/album\/[0-9]+\/track\/([0-9]+)/, id => `song.link/ya/${id}`),
         regexToLink(/.*music.yandex.ru\/album\/([0-9]+)/, id => `album.link/ya/${id}`),
 
+        // doesn't work at the moment (both quick link and sending it to the api)
         // regexToLink(/.*music.youtube.com\/browse\/(.+)/, id => `album.link/y/${id}`),
         regexToLink(/.*music.youtube.com\/playlist\?list=(.+)/, id => `album.link/y/${id}`),
         regexToLink(/.*music.youtube.com\/watch\?v=([^&]+)/, id => `song.link/y/${id}`),
 
         regexToLink(/.*deezer.com\/[a-z]{2,4}\/album\/([0-9]+)/, id => `album.link/d/${id}`),
-        regexToLink(/.*deezer.com\/[a-z]{2,4}\/track\/([0-9]+)/, id => `song.link/d/${id}`)
+        regexToLink(/.*deezer.com\/[a-z]{2,4}\/track\/([0-9]+)/, id => `song.link/d/${id}`),
+
+        regexToLink(/.*play.google.com\/music\/listen#\/album\/([^\/]+)/, id => `album.link/g/${id}`),
+        // pretty hard to obtain link in the moment via extension, but added to be consistent
+        regexToLink(/.*play.google.com\/music\/listen#\/track\/([^\/]+)/, id => `song.link/g/${id}`),
+
+        regexToLink(/.*open.spotify.com\/album\/(?:[^?]+)\?highlight=spotify:track:([^&]+)/, id => `song.link/s/${id}`),
+        regexToLink(/.*open.spotify.com\/album\/([^?]+)/, id => `album.link/s/${id}`),
+        // pretty hard to obtain link in the moment via extension, but added to be consistent
+        regexToLink(/.*open.spotify.com\/track\/([^?]+)/, id => `song.link/s/${id}`)
+
     ];
 
     let result;
